b3971c33324815089a49499eefe714807bf0805c
ribonsamr <amressam01@hotmail.com>
2020-10-14 05:59:25 +0200

    Implement edit_venue

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -337,6 +337,7 @@ def create_venue_submission():
         address=request.form['address'],
         phone=request.form['phone'],
         image_link=request.form['image_link'],
+        website=request.form['website_link'],
         genres=request.form.getlist('genres'),
         facebook_link=request.form['facebook_link'],
         seeking_talent=True
@@ -578,39 +579,61 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
     form = VenueForm()
-    venue = {
-        "id":
-            1,
-        "name":
-            "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address":
-            "1015 Folsom Street",
-        "city":
-            "San Francisco",
-        "state":
-            "CA",
-        "phone":
-            "123-123-1234",
-        "website":
-            "https://www.themusicalhop.com",
-        "facebook_link":
-            "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent":
-            True,
-        "seeking_description":
-            "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link":
-            "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    }
+    # venue = {
+    #     "id":
+    #         1,
+    #     "name":
+    #         "The Musical Hop",
+    #     "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
+    #     "address":
+    #         "1015 Folsom Street",
+    #     "city":
+    #         "San Francisco",
+    #     "state":
+    #         "CA",
+    #     "phone":
+    #         "123-123-1234",
+    #     "website":
+    #         "https://www.themusicalhop.com",
+    #     "facebook_link":
+    #         "https://www.facebook.com/TheMusicalHop",
+    #     "seeking_talent":
+    #         True,
+    #     "seeking_description":
+    #         "We are on the lookout for a local artist to play every two weeks. Please call us.",
+    #     "image_link":
+    #         "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+    # }
+    venue = Venue.query.get(venue_id)
     # TODO: populate form with values from venue with ID <venue_id>
     return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
-    # TODO: take values from the form submitted, and update existing
-    # venue record with ID <venue_id> using the new attributes
+    try:
+        venue = Venue.query.get(venue_id)
+        venue.name = request.form['name']
+        venue.city = request.form['city']
+        venue.state = request.form['state']
+        venue.address = request.form['address']
+        venue.phone = request.form['phone']
+        venue.image_link = request.form['image_link']
+        venue.website = request.form['website']
+        venue.genres = request.form.getlist('genres'),
+        venue.facebook_link = request.form['facebook_link']
+        venue.seeking_talent = True if request.form.get('seeking_talent',
+                                                        None) == 'y' else False
+        venue.seeking_description = request.form.get('seeking_description', "")
+        db.session.commit()
+        flash('Venue ' + request.form['name'] + ' was successfully updated!')
+    except:
+        print(sys.exc_info())
+        db.session.rollback()
+        flash('An error occurred. Venue ' + request.form['name'] +
+              ' could not be updated.')
+    finally:
+        db.session.close()
     return redirect(url_for('show_venue', venue_id=venue_id))
 
 
