36f071d22762786dc001ed2479eb4efc9bde334c
ribonsamr <amressam01@hotmail.com>
2020-10-14 03:37:55 +0200

    Added missing website field in venue model

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -45,6 +45,7 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(120))
