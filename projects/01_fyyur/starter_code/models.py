06eaaa7c65e3deca7307cc9136e7152911a234b2
ribonsamr <amressam01@hotmail.com>
2020-10-15 10:11:52 +0200

    Add SQL Constraints

--- projects/01_fyyur/starter_code/models.py
+++ projects/01_fyyur/starter_code/models.py
@@ -10,41 +10,43 @@ db = SQLAlchemy()
 class Show(db.Model):
     __tablename__ = 'Show'
     id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
-    artist = db.relationship("Artist", backref="shows")
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
-    venue = db.relationship("Venue", backref="shows")
-    start_time = db.Column(db.DateTime)
+    artist_id = db.Column(db.Integer,
+                          db.ForeignKey('Artist.id'),
+                          nullable=False)
+    artist = db.relationship("Artist", backref="shows", nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    venue = db.relationship("Venue", backref="shows", nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
 
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    website = db.Column(db.String(120))
-    facebook_link = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False, unique=True)
+    genres = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
+    website = db.Column(db.String(500), nullable=False, unique=True)
+    facebook_link = db.Column(db.String(500), nullable=False, unique=True)
+    seeking_talent = db.Column(db.Boolean, nullable=False)
+    seeking_description = db.Column(db.String(120), nullable=False)
 
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False, unique=True)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
+    phone = db.Column(db.String(120), nullable=False, unique=True)
+    genres = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(500), nullable=False, unique=True)
+    website = db.Column(db.String(500), nullable=False, unique=True)
+    seeking_venue = db.Column(db.Boolean, nullable=False)
+    seeking_description = db.Column(db.String(120), nullable=False)
