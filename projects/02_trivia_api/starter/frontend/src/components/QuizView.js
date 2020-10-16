2c805e8500b7078a3f73083c3b89ff84c925b615
ribonsamr <amressam01@hotmail.com>
2020-10-16 10:14:35 +0200

    Fix issue in categories frontend

--- projects/02_trivia_api/starter/frontend/src/components/QuizView.js
+++ projects/02_trivia_api/starter/frontend/src/components/QuizView.js
@@ -25,7 +25,7 @@ class QuizView extends Component {
       url: `/categories`, //TODO: update request URL
       type: "GET",
       success: (result) => {
-        this.setState({ categories: result });
+        this.setState({ categories: result.categories });
         return;
       },
       error: (error) => {
