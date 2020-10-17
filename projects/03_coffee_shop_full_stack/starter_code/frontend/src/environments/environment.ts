5df1da242f4d15fec19ecb9a1b8a511878ddc423
ribonsamr <amressam01@hotmail.com>
2020-10-17 10:13:29 +0200

    Edit auth0 url Ionic

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'rib-fsnd.eu.auth0.com', // the auth0 domain prefix
+    url: 'rib-fsnd.eu', // the auth0 domain prefix
     audience: 'coffeeapp', // the audience set for the auth0 app
     clientId: 'TccC5qaSlWLciviKMVqEi57yevBGIWDc', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:4200', // the base url of the running ionic application. 
